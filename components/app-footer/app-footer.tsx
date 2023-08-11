f60f4ddca91d003ce1b522724b5db2801c4ed859
AntonOshurek <oshurekweb@gmail.com>
2023-08-11 23:50:58 +0200

    add basic page structure

---
+++ components/app-footer/app-footer.tsx
@@ -0,0 +1,12 @@
+/* styles */
+import './app-footer.css';
+
+const AppFooter = () => {
+	return (
+		<footer className="app-footer container">
+			<p>footer</p>
+		</footer>
+	);
+};
+
+export default AppFooter;
---
+++ components/app-header/app-header.tsx
@@ -0,0 +1,12 @@
+/* styles */
+import './app-header.css';
+
+const AppHeader = () => {
+	return (
+		<header className="app-header container">
+			<h1>Top Films App</h1>
+		</header>
+	);
+};
+
+export default AppHeader;
