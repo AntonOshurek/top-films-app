b8e0d3aa70ac25e3a4e7fcdefae55116a8a227f6
AntonOshurek <oshurekweb@gmail.com>
2023-08-12 12:02:33 +0200

    create separate component for login button

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 import AppHeader from './app-header/app-header';
 import AppFooter from './app-footer/app-footer';
+import LoginBtn from './login-btn/login-btn';
 
-export { AppHeader, AppFooter };
+export { AppHeader, AppFooter, LoginBtn };
